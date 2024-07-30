1708f7d3eaa6b3091a0361585750923c621bd1b6
Serge-Olivier Malouin <supcakiod@gmail.com>
2024-07-29 22:01:40 -0400

    new default message

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, TypeScript API!');
+  res.send('hello world');
 });
 
 app.get('/health', (req, res) => {
