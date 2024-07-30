d275c4029a9d68cf2384694bc606cace19ac03a1
Serge-Olivier Malouin <supcakiod@gmail.com>
2024-07-29 21:54:59 -0400

    add health check

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import express from 'express';
+import { stat } from 'fs';
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello, TypeScript API!');
+});
+
+app.get('/health', (req, res) => {
+  try {
+    res.send('I am healty');
+  } catch (error) {
+    res.status(500).send('I am not healty');
+  }
 });
 
 app.listen(port, () => {
